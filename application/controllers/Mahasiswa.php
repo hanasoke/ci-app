148312e78b1c6241f02796f735d2525685384abf
hanasoke <hanasoke@gmail.com>
2019-10-14 23:29:56 +0700

    membuat tambah data dan hapus data pada tabel mahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -7,6 +7,7 @@ class Mahasiswa extends CI_Controller {
     {
         parent::__construct();
         $this->load->model('Mahasiswa_model');
+        $this->load->library('form_validation');
     }
     
     public function index()
@@ -16,5 +17,37 @@ class Mahasiswa extends CI_Controller {
         $this->load->view('templates/header', $data);
         $this->load->view('mahasiswa/index', $data);
         $this->load->view('templates/footer');
+    }
+
+    public function tambah()
+    {
+        $data['head'] = 'Form Tambah Data Mahasiswa';
+
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('nim', 'NIM', 'required|numeric');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+        
+
+        if ($this->form_validation->run() == FALSE ) {
+            $data['head'] = 'Form Tambah Data Mahasiswa';
+            $this->load->view('templates/header', $data);
+            $this->load->view('mahasiswa/tambah');
+            $this->load->view('templates/footer');
+        } else {
+            // test
+            // echo "Berhasil!";
+
+            // insert ke database
+            $this->Mahasiswa_model->tambahDataMahasiswa();
+            $this->session->set_flashdata('flash', 'Ditambahkan');
+            redirect('mahasiswa');
+        }
+    }
+
+    public function hapus($id)
+    {
+        $this->Mahasiswa_model->hapusDataMahasiswa($id);
+        $this->session->set_flashdata('flash', 'Dihapus');
+        redirect('mahasiswa');
     }
 }
