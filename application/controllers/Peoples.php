9c99bdeb08d8d22ed0010b11a937f2608cf49874
hanas_bp <hanasoke@gmail.com>
2019-10-28 23:11:58 +0700

    menambahkan fitur searching dan mengubah susunan code pagination

--- application/controllers/Peoples.php
+++ application/controllers/Peoples.php
@@ -13,49 +13,47 @@ class Peoples extends CI_Controller {
         // load library
         $this->load->library('pagination');
 
+
+        // ambil data keyword
+        // if ($this->input->post('submit')) {
+        //     echo $this->input->post('keyword');
+        //     die;
+        // }
+
+        if ($this->input->post('submit')) {
+            $data['keyword'] = $this->input->post('keyword');
+            $this->session->set_userdata('keyword', $data['keyword']);
+
+        } else {
+            // kalau ambil semua data tampa session gunakan null
+            // $data['keyword'] = null;
+
+            // cara ambil data menggunakan session
+            $data['keyword'] = $this->session->userdata('keyword');
+        }
+
         // Config
-        $config['base_url'] = 'http://localhost/ci-app/peoples/index';
-        $config['total_rows'] = $this->peoples->countAllPeoples();
+
+        // untuk mencari semua orang
+        // $config['total_rows'] = $this->peoples->countAllPeoples();
+
         // var_dump($config['total_rows']);
         // die;
-        $config['per_page'] = 12;
-        $config['num_links'] = 5;
 
-        // styling
-        $config['full_tag_open'] = '<nav><ul class="pagination pagination-lg justify-content-center">';
-
-        $config['full_tag_close'] = '</ul></nav>';
-
-        $config['first_link'] = 'First';
-        $config['first_tag_open'] = '<li class="page-item">';
-        $config['first_tag_close'] = '</li>';
-
-        $config['last_link'] = 'Last';
-        $config['last_tag_open'] = '<li class="page-item">';
-        $config['last_tag_close'] = '</li>';
-
-        $config['next_link'] = '&raquo';
-        $config['next_tag_open'] = '<li class="page-item">';
-        $config['next_tag_close'] = '</li>';
-
-        $config['prev_link'] = '&laquo';
-        $config['prev_tag_open'] = '<li class="page-item">';
-        $config['prev_tag_close'] = '</li>';
-
-        $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link" href="#">';
-        $config['cur_tag_close'] = '</a></li>';
-
-        $config['num_tag_open'] = '<li class="page-item">';
-        $config['num_tag_close'] = '</li>';
-
-        $config['attributes'] = array('class' => 'page-link');
+        // untuk mencari orang berdasarkan pencarian
+        $this->db->like('name', $data['keyword']);
+        $this->db->or_like('email', $data['keyword']);
+        $this->db->from('peoples');
+        $config['total_rows'] = $this->db->count_all_results();
+        $data['total_rows'] = $config['total_rows'];
+        $config['per_page'] = 8;
 
         // initialize
         $this->pagination->initialize($config);
 
 
         $data['start'] = $this->uri->segment(3);
-        $data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start']);
+        $data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start'], $data['keyword']);
         
         $this->load->view('templates/header', $data);
         $this->load->view('peoples/index', $data);
